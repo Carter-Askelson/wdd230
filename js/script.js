66ffd7bece5aa7ea967b74861ab164d7b3746a81
Carter-Askelson <Askelsonc@gmail.com>
2021-09-18 03:45:23 -0600

    more js fix

--- js/script.js
+++ js/script.js
@@ -8,6 +8,6 @@ function newdate() {
         hourtext = hour - 12;
         timeofday = "PM";
     }
-    document.getElementById("copyright").innerHTML = date.getFullYear();
+    document.getElementById("copyright").innerHTML = currentdate.getFullYear();
     document.getElementById("lastupdate").innerHTML = document.lastModified;
 }
