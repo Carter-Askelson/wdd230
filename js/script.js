5b4d247bff5a7a0e6cdba43c45fd4824a259b3f4
Carter-Askelson <Askelsonc@gmail.com>
2021-09-18 04:05:36 -0600

    missed part

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let date = new Date()
 
 function newdate() {
-    let hour = currentdate.getHours();
+    let hour = date.getHours();
     var hourtext = hour;
     var timeofday = "AM";
     if (hour > 12) {
         hourtext = hour - 12;
         timeofday = "PM";
     }
-    document.getElementById("copyright").innerHTML = currentdate.getFullYear();
+    document.getElementById("copyright").innerHTML = date.getFullYear();
     document.getElementById("lastupdate").innerHTML = document.lastModified;
 }
