78ac96754a4a551da18d2cca1149e801390fbab8
Carter-Askelson <Askelsonc@gmail.com>
2021-10-16 17:03:02 -0600

    trial 1

--- lesson5/js/js.js
+++ lesson5/js/js.js
@@ -17,3 +17,20 @@ function newdate() {
 function toggleMenu() {
     document.getElementsByClassName("nav_bar")[0].classList.toggle("responsive");
 }
+
+const day_list = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const month_list = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var new_date = new Date();
+var day = new_date.getDay()
+var month = new_date.getMonth()
+var day = new_date.getFullYear()
+var date = new_date.getDate()
+var dateString = day_list[day] + ", " + date + " " + month_list[month] + " " + year;
+
+if (day_list[day] == "Thursday") {
+    document.getElementById("annoucement").innerHTML = "Friday = State mandated lockdown due to Ice Storm Issac.";
+    document.getElementById("annoucement").style.display = "block";
+}
+else {
+    document.getElementById("annoucement").style.display = "none";
+}
