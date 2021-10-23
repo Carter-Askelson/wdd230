6c494ee48490a586c3c7667e9627ccf869cb559b
Carter Askelson <62883098+Carter-Askelson@users.noreply.github.com>
2021-10-23 01:44:57 -0600

    Update windchill_test.js

--- lesson6/js/windchill_test.js
+++ lesson6/js/windchill_test.js
@@ -1,3 +1,7 @@
+const windspeed = document.getElementById('windspeed').innerHTML;
+const temp = document.getElementById('temp').innerHTML;
+
+
 let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * Math.pow(windspeed, .16));
     if (temp <= 50 && windspeed > 3) {
         windchill = Math.round(windchill);
