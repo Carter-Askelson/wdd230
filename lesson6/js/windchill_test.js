8924850fcc9e3074929045cd36281b136fb82499
Carter-Askelson <Askelsonc@gmail.com>
2021-10-23 00:48:23 -0600

    lesson 6

---
+++ lesson6/js/windchill_test.js
@@ -0,0 +1,7 @@
+let windchill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * Math.pow(windspeed, .16));
+    if (temp <= 50 && windspeed > 3) {
+        windchill = Math.round(windchill);
+    } else {
+        windchill = "Minimal";
+    }
+document.getElementById('windchill').innerHTML = windchill;
