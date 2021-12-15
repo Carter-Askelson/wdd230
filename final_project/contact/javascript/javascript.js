02c4297322b22a0f8c4dd79ce4851ec6456a8500
Carter Askelson <62883098+Carter-Askelson@users.noreply.github.com>
2021-12-14 17:09:55 -0700

    Update javascript.js

--- final_project/contact/javascript/javascript.js
+++ final_project/contact/javascript/javascript.js
@@ -1,1 +1,15 @@
+let currentdate = new Date();
 
+
+function newdate() {
+
+    let hour = currentdate.getHours();
+    var hourtext = hour;
+    var timeofday = 'AM';
+    if (hour > 12) {
+        hourtext = hour - 12;
+        timeofday = 'PM';
+    }
+    document.getElementById("copyright").innerHTML = currentdate.getFullYear();
+    document.getElementById("lastupdate").innerHTML = document.lastModified;
+}
